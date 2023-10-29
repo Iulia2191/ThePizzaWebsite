9232e550e785ff347781ff704e0bf2dfecc23835
Iulia2191 <ibocaniciu@gmail.com>
2023-10-29 18:22:34 +0100

    home page

--- the-pizza/src/Components/Footer/Footer.jsx
+++ the-pizza/src/Components/Footer/Footer.jsx
@@ -3,14 +3,13 @@ import { FaFacebook } from 'react-icons/fa'
 
 function Footer () {
   return (
-    <div className='footer px-5'>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-12 col-sm-6 col-lg-6'>
-            <h3 className='h3'>The Pizza</h3>
-            <h4 className='h4'>Napolitan Style Pizza</h4>
+    <div className='footer py-3'>
+      <div className='container-fluid footer-container'>
+          <div className='col1'>
+            <h3 className='title'>The Pizza</h3>
+            <h4 className='nap'>Napolitan Style Pizza</h4>
           </div>
-          <div className='col-12 col-sm-6 col-lg-6'>
+          <div className='col2'>
             <p>
               <span className='material-symbols-outlined px-2'>
                 location_on
@@ -24,15 +23,19 @@ function Footer () {
               Vorbestellungen: +43 660 1711525
             </p>
             <p className='px-2'>
-              <span className='px-2 facebook'>
-                <FaFacebook />
-              </span>
-              Termine werden immer aktuell gepostet{' '}
+              <a href='https://www.facebook.com/profile.php?id=61552396569253'>
+                <span className='px-2 facebook'>
+                  <FaFacebook />
+                </span>
+              </a>
+              Termine werden immer aktuell gepostet
             </p>
-          </div>
+        
         </div>
       </div>
-      <p>&#169; Copyright 2023 The Pizza. All Rights Reserved</p>
+      <p className='text-center py-2'>
+        &#169; Copyright 2023 The Pizza. All Rights Reserved
+      </p>
     </div>
   )
 }
