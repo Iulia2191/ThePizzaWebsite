53850088b75a888213d545d538c6ef93739ad303
Iulia2191 <ibocaniciu@gmail.com>
2023-10-29 18:02:32 +0100

    fb

--- the-pizza/src/Components/Footer/Footer.jsx
+++ the-pizza/src/Components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import './Footer.css'
-import { FaFacebook } from 'react-icons/fa';
-
+import { FaFacebook } from 'react-icons/fa'
 
 function Footer () {
   return (
@@ -10,18 +9,26 @@ function Footer () {
           <div className='col-12 col-sm-6 col-lg-6'>
             <h3 className='h3'>The Pizza</h3>
             <h4 className='h4'>Napolitan Style Pizza</h4>
-           
           </div>
           <div className='col-12 col-sm-6 col-lg-6'>
             <p>
-              <span className='material-symbols-outlined'>location_on</span>
-              Adresse: Bahnhofstraße 24, Parkplatz Ecke Farben Morscher{' '}
+              <span className='material-symbols-outlined px-2'>
+                location_on
+              </span>
+              Adresse: Bahnhofstraße 24, Parkplatz Ecke Farben Morscher
             </p>
             <p>
-              <span className='material-symbols-outlined'>phone_iphone</span>{' '}
+              <span className='material-symbols-outlined px-2'>
+                phone_iphone
+              </span>
               Vorbestellungen: +43 660 1711525
             </p>
-            <p><FaFacebook/>Termine werden immer aktuell gepostet </p>
+            <p className='px-2'>
+              <span className='px-2 facebook'>
+                <FaFacebook />
+              </span>
+              Termine werden immer aktuell gepostet{' '}
+            </p>
           </div>
         </div>
       </div>
