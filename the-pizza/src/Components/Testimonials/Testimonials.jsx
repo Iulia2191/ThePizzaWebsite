57cf92d5257a547c4279d61e03bae37f0bda4e22
Iulia- Elena Pop <125976840+Iulia2191@users.noreply.github.com>
2023-11-01 10:26:47 +0100

    Update Testimonials.jsx

--- the-pizza/src/Components/Testimonials/Testimonials.jsx
+++ the-pizza/src/Components/Testimonials/Testimonials.jsx
@@ -5,8 +5,8 @@ import Testimonial from '../Carousel/Carousel'
 function Testimonials () {
   return (
     <div className='testimonials text-center pt-5'>
-        <h2 className='text-light pb-3'>Was die Leute über uns sagen</h2>
-      <div className='container-fluid py-3 px-5'>
+        <h2 className='text-light py-3'>Was die Leute über uns sagen</h2>
+      <div className='container-fluid px-5'>
         <Testimonial/>
       </div>
     </div>
