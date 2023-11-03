511df6471387d629bbfed0c9091b619474493ea6
Iulia2191 <ibocaniciu@gmail.com>
2023-11-03 14:53:48 +0100

    gallery

--- the-pizza/src/pages/Gallery/Gallery.jsx
+++ the-pizza/src/pages/Gallery/Gallery.jsx
@@ -1,22 +1,29 @@
+import { useState } from 'react'
 import { Media } from '../../Components/Media/Media'
 import './Gallery.scss'
 
 function Gallery () {
+  const [file, setfile] = useState(null)
   return (
     <div className='gallery'>
       <div className='gallery-container'>
         <div className='gallery-title'>
-        <h2>GALERIE</h2>
+          <h2>GALERIE</h2>
         </div>
-       
+
         <div className='media-container px-2'>
           {Media.map((file, index) => (
-            <div className='media' key={index}>
+            <div className='media' key={index} onClick={() => setfile(file)}>
               {file.type === 'img' ? (
                 <img src={file.url} alt={file.alt} />
               ) : null}
             </div>
           ))}
+        </div>
+        <div className='popup-media' style={{display: file? 'block' : 'none'}}>
+          <span className='remove' onClick={ () => setfile(null)}>&times;</span>
+
+          {file?.type === 'img' ? <img src={file?.url} /> : null}
         </div>
       </div>
     </div>
