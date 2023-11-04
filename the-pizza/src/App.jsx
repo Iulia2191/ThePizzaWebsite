4fe77ade9150c1edb68debb7cc09aeac8e1f0a80
Iulia2191 <ibocaniciu@gmail.com>
2023-11-04 13:02:24 +0100

    remove css

--- the-pizza/src/App.jsx
+++ the-pizza/src/App.jsx
@@ -13,6 +13,7 @@ import Footer from "./Components/Footer/Footer";
 import Prosciutto from "./pages/Prosciutto/Prosciutto";
 import Credits from "./pages/Credits/Credits";
 
+
 const Layout = () =>{
   return (
     <div className="app">
