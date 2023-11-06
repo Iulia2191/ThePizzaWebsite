dad2e1da3dcdc0b9ab28681f6aeee705f5875015
Iulia2191 <ibocaniciu@gmail.com>
2023-11-06 13:00:01 +0100

    updated gallery

--- the-pizza/src/Components/Media/Media.js
+++ the-pizza/src/Components/Media/Media.js
@@ -1,23 +1,23 @@
-import salamiImage from './salami.jpg';
-import prosImage from './pros.jpg';
-import ovenImage from './oven.jpg';
-import prosciuttoImage from './prosciutto.jpg';
-import margheritaImage from './margherita.jpg';
+import salamiImage from './salami.jpg'
+import prosImage from './pros.jpg'
+import ovenImage from './oven.jpg'
+import prosciuttoImage from './prosciutto.jpg'
+import margheritaImage from './margherita.jpg'
 import calzoneImage from './calzone.jpg'
 import doughImage from './pizza-dough.jpg'
 import agliImage from './agli.jpg'
 import hawaiiImage from './hawaii.jpg'
-
+import salami2Image from './salami.jpg'
 
 export const Media = [
   { type: 'img', url: salamiImage, alt: 'Salami Pizza' },
   { type: 'img', url: prosImage, alt: 'Prosciutto Crudo Pizza' },
   { type: 'img', url: ovenImage, alt: 'Pizza Oven' },
-  { type: 'img', url: prosciuttoImage, alt: 'Prosciutto Crudo Pizza'},
-  { type: 'img', url: margheritaImage, alt: 'Margherita Pizza'},
-  { type: 'img', url: calzoneImage, alt: 'Calzone Pizza'},
-  { type: 'img', url: doughImage, alt: 'Pizza dough'},
-  { type: 'img', url: agliImage, alt: 'Agli spinaci Pizza'},
-  { type: 'img', url: hawaiiImage, alt: 'Spicy Hawaii Pizza'},
-
-];
+  { type: 'img', url: prosciuttoImage, alt: 'Prosciutto Crudo Pizza' },
+  { type: 'img', url: margheritaImage, alt: 'Margherita Pizza' },
+  { type: 'img', url: calzoneImage, alt: 'Calzone Pizza' },
+  { type: 'img', url: doughImage, alt: 'Pizza dough' },
+  { type: 'img', url: agliImage, alt: 'Agli spinaci Pizza' },
+  { type: 'img', url: hawaiiImage, alt: 'Spicy Hawaii Pizza' },
+  { type: 'img', url: salami2Image, alt: 'Salami Pizza' }
+]
